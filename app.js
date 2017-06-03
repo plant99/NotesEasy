bd21f7080455548094cc87eba1bdc9fae5f22fd9
shivashis <shivashispadhi@gmail.com>
2017-06-03 14:14:42 +0530

    Remove unnecessary error page generators

--- app.js
+++ app.js
@@ -52,22 +52,12 @@ app.use('/getContent/:id',function(req,res){
 	})
 })
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.use(function(req,res){
+	res.render('error',{
+		message: "God knows what's going on. What about you troubleshoot me at:",
+		email: "shivashispadhi@gmail.com"
+	})
+})
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
